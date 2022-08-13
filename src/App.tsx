c8f21dec819824c00a0fd2c22c29bbd2779a40b8
ramirezmz <roberto.paolo@dremio.com>
2022-08-13 11:23:56 -0300

    feat: create routes to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,10 @@
 import api from './services/api'
 import Navbar from './components/NavBar/Navbar'
 import './main.scss'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home/Home'
+import Contact from './pages/Contact/Contact'
+import Tech from './pages/About/About'
 
 const App = () => {
   
@@ -11,11 +15,14 @@ const getData = () => {
 }
 
   return (
-    <div className="App">
+    <Router>
       <Navbar />
-      hello
-      <button onClick={getData}>Get Data</button>
-    </div>
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/contact' element={<Contact/>} />
+        <Route path='/about' element={<Tech/>} />
+      </Routes>
+    </Router>
   )
 }
 
