948a833ae667b912d210ceeab86cf3a625e9c55b
ramirezmz <roberto.paolo@dremio.com>
2022-08-16 16:24:42 -0300

    feat: add routes to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import api from './services/api'
 import Navbar from './components/NavBar/Navbar'
 import './main.scss'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
@@ -7,12 +6,6 @@ import Contact from './pages/Contact/Contact'
 import Tech from './pages/About/About'
 
 const App = () => {
-  
-const getData = () => {
-  api.get('/').then(response => {
-    console.log(response.data)
-  })
-}
 
   return (
     <Router>
