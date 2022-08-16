fee411b1cbced16721e99b92e3ef8cf17b5bcd03
ramirezmz <roberto.paolo@dremio.com>
2022-08-16 16:19:19 -0300

    feat: create loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './_Loading.scss'
+function Loading() {
+  return (
+    <div className="lds-roller">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
+
+export default Loading
