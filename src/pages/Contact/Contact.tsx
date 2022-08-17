5f4f31453b8a275d2c644b960c8b35bcb559e4dc
ramirezmz <roberto.paolo@dremio.com>
2022-08-16 21:42:07 -0300

    fix: clean contact page

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,18 +1,9 @@
 import React from 'react'
 import '../_Pages.scss'
-import Card from '../../components/Card/Card'
 function Contact() {
   return (
     <div className='container'>
       <h1>CONTACT</h1>
-      <Card 
-        country='Brazil'
-        capital_city='Brasilia'
-        administered='100'
-        people_vaccinated='100'
-        people_partially_vaccinated='100'
-        population='100'
-      />
     </div>
   )
 }
